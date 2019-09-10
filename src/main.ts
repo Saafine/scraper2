cc78ba5ed8ffcaa8a350d14e4908f81b6977f38f
Pawel Lbs <pawel.lbs@gmail.com>
2019-09-10 20:02:01 +0200

    X

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+    const fs = require('fs');
+
+    const keyPath = process.env.KEY_PATH || 'NO KEY PATH';
+    const certPath = process.env.CERT_PATH || 'NO CERT PATH';
+    console.log('Bootstrapping SSL', { keyPath, certPath });
+    const keyFile = fs.readFileSync(keyPath);
+    const certFile = fs.readFileSync(certPath);
+
+    const app = await NestFactory.create(AppModule, {
+        httpsOptions: {
+            key: keyFile,
+            cert: certFile,
+        }
+    });
 }
+
 bootstrap();
