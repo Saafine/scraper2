99dcf54215b19ba8bf496cc684d242e135d276b1
Pawel Lbs <pawel.lbs@gmail.com>
2019-09-12 19:45:14 +0200

    X

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const fs = require('fs');
-
     const keyPath = process.env.KEY_PATH || 'NO KEY PATH';
     const certPath = process.env.CERT_PATH || 'NO CERT PATH';
     console.log('Bootstrapping SSL', { keyPath, certPath });
     const keyFile = fs.readFileSync(keyPath);
     const certFile = fs.readFileSync(certPath);
-
     const app = await NestFactory.create(AppModule, {
         httpsOptions: {
             key: keyFile,
             cert: certFile,
         }
     });
+    await app.listen(process.env.PORT || 3000);
 }
 
 bootstrap();
